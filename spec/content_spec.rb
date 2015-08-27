8138d2b23537e5409834e9e35500fd225ef5263f
Andre Heijstek <andre.heijstek@improvementfocus.com>
2015-08-27 17:29:59 +0200

    Update spec - now it fails properly -;)

--- spec/content_spec.rb
+++ spec/content_spec.rb
@@ -3,7 +3,8 @@ describe Content do
   it 'should #get_from_website' do
     content = Content.new({title: "dummy", notes: "dummy"})
     text = content.get_from_website
-    puts "Text from website: #{text}"
+    puts "\nText from website: #{text}"
     text.should.not.equal ""
+    text.class.should.equal String
   end
 end
