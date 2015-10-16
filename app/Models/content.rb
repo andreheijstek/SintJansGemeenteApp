d81f4947f0911f09e7b8d5a3b6d6924f947b633b
Andre Heijstek <andre.heijstek@improvementfocus.com>
2015-10-16 15:00:55 +0200

    Cleanup in NewsletterWebsite

--- app/Models/content.rb
+++ app/Models/content.rb
@@ -8,18 +8,15 @@ class Content
   def initialize(properties = {})
     properties.each do |key, value|
       if PROPERTIES.member? key.to_sym
-        self.send("#{key}=", value.to_s) # Note: the to_s was needed, because when clicking on a Content line with the
-                                         #       :note as an integer crashed the app
+        self.send("#{key}=", value.to_s)
+        # Note: the to_s was needed, because when clicking on a Content line with the :note as an integer crashed the app
       end
     end
   end
 
-
   def self.all
     @@all
   end
-
-  # TODO Ipv set_all, een all= method maken
 
   def self.all=(input)
     input.each do |k, v|
