02d47c8c13587d60b72fb99a2b023337bdd9c9b5
Andre Heijstek <andre.heijstek@improvementfocus.com>
2015-10-16 22:49:49 +0200

    Refactor Content unit tests

--- spec/content_spec.rb
+++ spec/content_spec.rb
@@ -1,42 +1,34 @@
 describe Content do
 
-  it 'should #initialize' do
-    content = Content.new({title: "dummy", notes: "dummy"})
-    content.class.should.equal Content
-  end
+  describe '#all=' do
 
-  # Ik moet nu voor ieder element in de data hash een Content instance maken
-  # Een content instance verwacht een hash met 2 elementen: title: en notes:
-  # data bevat allemaal verschillende keys, zoals id:, time1:, time2: welcome:, etc
-  # Dat lijkt me een mooi klusje voor de Content class zelf
-  # Dat kan dan eindelijk ook weer eens test-driven
-  # Content.set_all(data) => content.all met allemaal losse content instances
-
-  it 'should #set all with a hash of length 0' do
-    Content.all = {}
-    puts "\nContent.all = #{Content.all}"
-    Content.all.length.should.equal 0
-    Content.clear_all
-  end
-
-  it 'should #set all with a hash of length 1' do
-    Content.all = {id: 1}
-    puts "\nContent.all = #{Content.all}"
-    Content.all.length.should.equal 1
-    Content.all.each do |content|
-      content.title.should.equal "id"
-      content.notes.should.equal "1"
+    after do
+      Content.clear_all
     end
-    Content.clear_all
+
+    it 'should set #all with a hash of length 0' do
+      Content.all = {}
+      Content.all.length.should.equal 0
+    end
+
+    it 'should set #all with a hash of length 1' do
+      Content.all = {id: 1}
+      Content.all.length.should.equal 1
+      Content.all.each do |content|
+        content.title.should.equal "id"
+        content.notes.should.equal "1"
+      end
+    end
+
+    it 'should set #all with a hash of length 2' do
+      Content.all = {id: 1, something: 'else'}
+      Content.all.length.should.equal 2
+      Content.all[0].class.should.equal Content
+      Content.all[0].title.should.equal "id"
+      Content.all[0].notes.should.equal "1"
+      Content.all[1].title.should.equal "something"
+      Content.all[1].notes.should.equal "else"
+    end
   end
 
-  it 'should #set all with a hash of length 2' do
-    Content.all = {id: 1, something: 'else'}
-    puts "\nContent.all = #{Content.all}"
-    Content.all.length.should.equal 2
-    Content.all[0].class.should.equal Content
-    Content.all[0].title.should.equal "id"
-    Content.all[1].notes.should.equal "else"
-    Content.clear_all
-  end
 end
